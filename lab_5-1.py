11d1a783a7a2e08ae61debf99fb4790a36041513
Ryan Stewart <P22rstewart@fairfieldprep.org>
2021-10-06 10:08:10 -0400

    .

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: Ryan (AMDG) 10/4/21
+
+#int(a)
+#name error
+
+#int('a')
+#value error
+
+#'a' + 2
+#type error
+
+#import date
+#modulenotfound
+
+#print("I'm a happy camper!)
+#syntax error
